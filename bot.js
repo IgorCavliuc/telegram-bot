77c9c13f2ba8a98e4d9bde0ba06e66d1a644a6e5
Cavliuc Igor <cavliucigor@Cavliucs-MacBook-Pro.local>
2023-08-25 22:43:15 +0300

    ..

--- bot.js
+++ bot.js
@@ -1,7 +1,8 @@
 require("dotenv").config();
-const { Telegraf, Markup } = require("telegraf");
+const { Telegraf } = require("telegraf");
+
 const token = process.env.TELEGRAN_BOT_TOKEN;
-// const webhookUrl = process.env.WEBHOOK_URL;
+const webhookUrl = process.env.WEBHOOK_URL;
 
 const bot = new Telegraf(token);
 
@@ -22,6 +23,14 @@ let root = {
   auth: false,
 };
 
+async function setWebhook() {
+  try {
+    const result = await bot.telegram.setWebhook(webhookUrl);
+    console.log("Webhook was set successfully:", result);
+  } catch (error) {
+    console.error("Error setting up webhook:", error);
+  }
+}
 
 const commands = [
   { command: "/start", description: "Начальное приветствие." },
@@ -40,76 +49,65 @@ const commands = [
 
 
 bot.start(async (ctx) => {
-  const chatId = ctx.chat.id;
+  const userId = ctx.from?.id;
+  const userName = "@" + ctx.from?.username;
 
   const keyboard = {
     reply_markup: {
       keyboard: commands.map(command => [{ text: command.command }]),
-      resize_keyboard: true
+      one_time_keyboard: true
     }
   };
 
-  await ctx.reply('Выберите команду:', keyboard);
-});
+  await updateUser(userName, userId);
 
-// bot.start(async (ctx) => {
-//   const userId = ctx.from?.id;
-//   const userName = "@" + ctx.from?.username;
-//   await updateUser(userName, userId);
-//
-//   const brotherList = await getUser();
-//
-//   const dataUser = brotherList.filter(
-//     (bro) => bro.nickname.split("@")[1] === ctx.from.username
-//   );
-//
-//   root = {
-//     auth: dataUser.length !== 0,
-//     admin: dataUser[0]?.admin,
-//   };
-//
-//   root.auth
-//     ? handleStartCommand(bot, ctx, root)
-//     : handleStartCommand(bot, ctx, root, ctx.from.first_name);
-//
-//   // const users = brotherList.filter((user) => typeof user?.user_id === "number");
-//
-//   // Function to send a message to each user
-// });
+  const brotherList = await getUser();
+
+  const dataUser = brotherList.filter(
+      (bro) => bro.nickname.split("@")[1] === ctx.from.username
+  );
+
+  root = {
+    auth: dataUser.length !== 0,
+    admin: dataUser[0]?.admin,
+  };
+
+  root.auth
+      ? handleStartCommand(bot, ctx, root)
+      : handleStartCommand(bot, ctx, root, ctx.from.first_name);
+});
 
 bot.help((ctx) => {
   root.auth
-    ? handleHelpCommand(bot, ctx, root)
-    : handleHelpCommand(bot, ctx, root, ctx.from.first_name);
+      ? handleHelpCommand(bot, ctx, root)
+      : handleHelpCommand(bot, ctx, root, ctx.from.first_name);
 });
 
 bot.command("add_task", (ctx) => {
-  // root?.admin
-  // ?
   handleAddCommand(bot, ctx);
-  // : ctx.reply(
-  //     `Брат ${ctx.from.first_name}, у тебя нет доступа к этой команде, если ты хочешь добавить/изменить/удалить график, пожалуйста обратись к назначеному брату`
-  //   );
 });
 
 bot.command("add_bro", (ctx) => {
   handleAddBroCommandAdmin(bot, ctx, root, ctx.from.first_name);
 });
+
 bot.command("list_schedule", (ctx) => {
   root?.auth
-    ? handleGetSchedule(bot, ctx)
-    : ctx.reply(
-        `${ctx.from.first_name}, у тебя нет доступа к этой команде, если ты хочешь просмотреть данные этой команды, пожалуйста обратись к назначеному брату`
+      ? handleGetSchedule(bot, ctx)
+      : ctx.reply(
+          `${ctx.from.first_name}, у тебя нет доступа к этой команде, если ты хочешь просмотреть данные этой команды, пожалуйста обратись к назначеному брату`
       );
 });
+
 bot.command("list_brothers", (ctx) => {
   root?.auth
-    ? handleGetBrother(bot, ctx)
-    : ctx.reply(
-        `${ctx.from.first_name}, у тебя нет доступа к этой команде, если ты хочешь просмотреть данные этой команды, пожалуйста обратись к назначеному брату`
+      ? handleGetBrother(bot, ctx)
+      : ctx.reply(
+          `${ctx.from.first_name}, у тебя нет доступа к этой команде, если ты хочешь просмотреть данные этой команды, пожалуйста обратись к назначеному брату`
       );
 });
 
 bot.launch().then(() => {
   console.log("Bot started");
+  setWebhook();
 });
